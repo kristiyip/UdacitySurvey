35201ec8dde588a6ac957988f02a0105cc35c2cf
Kristin Diep <kristindiep@Kristins-MBP.fios-router.home>
2020-05-30 16:26:12 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User'); //Must be required before ./services/passport so that it's defined and passport can use it
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ status: 'in progress' });
-});
+app.use(
+    cookieSession({
+        // cookie lasts for 30 days
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
